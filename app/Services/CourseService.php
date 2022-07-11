41a9e42da3e66bc83b63f5718c25cec72061ed81
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2022-07-11 11:02:17 -0300

    feature: criadas rotas para CRUD de courses

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -2,10 +2,40 @@
 
 namespace App\Services;
 
+use App\Repositories\CourseRepository;
+
 class CourseService 
 {
+
+    protected $repository;
+
+    public function __construct(CourseRepository $courseRepository)
+    {
+        $this->repository = $courseRepository;
+    }
+
     public function getCourses()
     {
-        
+        return $this->repository->getAllCourses();
+    }
+
+    public function createNewCourse(array $data) 
+    {
+        return $this->repository->createNewCourse($data);
+    }
+
+    public function getCourse(string $identify)
+    {
+        return $this->repository->getCourseByUuid($identify);
+    }
+
+    public function deleteCourse(string $identify)
+    {
+        $this->repository->deleteCourseByUuid($identify);
+    }
+
+    public function updateCourse(string $identify, array $data)
+    {
+        $this->repository->updateCourseByUuid($identify, $data);
     }
 }
